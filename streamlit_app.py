3123653fd4abe613cc0464e887d396741dbce3f7
AlphaBah93 <amadalpha93@gmail.com>
2025-11-19 18:41:01 +0100

    Add DataFrame creation to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My new Streamlit app")
 st.write("Let's start building! For help and inspiration --- My name : Alpha Amadou BAH")
 st.text("Alpha Amadou BAH - Data Analyst")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
