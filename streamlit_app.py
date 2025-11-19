4417aec4cb986e8b9bb7aa6d51a034eeac7c0e4b
AlphaBah93 <amadalpha93@gmail.com>
2025-11-19 18:35:38 +0100

    Add data analyst title to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("My new Streamlit app")
 st.write("Let's start building! For help and inspiration --- My name : Alpha Amadou BAH")
+st.text("Alpha Amadou BAH - Data Analyst")
