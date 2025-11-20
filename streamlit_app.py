a761cf40572997bcdb0c79b20082ba59cb88307e
AlphaBah93 <amadalpha93@gmail.com>
2025-11-20 17:26:34 +0100

    Refactor Styler example code and update map data display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,18 @@ import numpy as np
 #st.write("Let's start building! For help and inspiration")
 #st.text("Alpha Amadou BAH - Data Analyst")
 
-#Styler
+#Styler ------------------
 # st.title("Styler Example")
 # dataframe = pd.DataFrame(np.random.randn(10, 20), columns=('col %d' % i for i in range(20)))
 # st.dataframe(dataframe.style.highlight_max(axis=0))
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-st.write(chart_data.style.highlight_max(axis=0))
+# Line chart with Styler ------------------
+# chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+# st.write(chart_data.style.highlight_max(axis=0))
+# st.line_chart(chart_data)
 
-st.line_chart(chart_data)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
