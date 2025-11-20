6af57431a43aba13d9f35fe6b43836c229ec23d2
AlphaBah93 <amadalpha93@gmail.com>
2025-11-21 00:26:14 +0100

    Refactor Selectbox widget example and add slider to sidebar in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,20 @@ import numpy as np
 #    st.dataframe(chart_data)  # Same as st.write(df)
 
 # Selectbox widget ------------------
-st.title("Selectbox Example")
-df = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]})
+#st.title("Selectbox Example")
+#df = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]})
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+#option = st.selectbox(
+#    'Which number do you like best?',
+#     df['first column'])
+#'You selected: ', option
 
-'You selected: ', option
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
