47845c9e8484ea7fe3709d72583a4aa37af450a4
AlphaBah93 <amadalpha93@gmail.com>
2025-11-20 16:56:40 +0100

    Refactor Streamlit app by removing unused DataFrame variable and cleaning up code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
+"""
 st.title("My new Streamlit app")
 st.write("Let's start building! For help and inspiration")
 st.text("Alpha Amadou BAH - Data Analyst")
+"""
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
