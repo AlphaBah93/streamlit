7d4275574c400825e623ac14aeeda97b64c199a6
AlphaBah93 <amadalpha93@gmail.com>
2025-11-20 16:58:13 +0100

    Remove commented-out introductory messages from Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-"""
-st.title("My new Streamlit app")
-st.write("Let's start building! For help and inspiration")
-st.text("Alpha Amadou BAH - Data Analyst")
-"""
+#st.title("My new Streamlit app")
+#st.write("Let's start building! For help and inspiration")
+#st.text("Alpha Amadou BAH - Data Analyst")
 
 
 st.write("Here's our first attempt at using data to create a table:")
